2d54c4fd30a530b980d5f761b47f53dab44fbbbf
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-02-20 16:47:39 +0100

    Update enforce_https.py

--- enforce_https.py
+++ enforce_https.py
@@ -1,8 +1,40 @@
+from __future__ import annotations
+
+import argparse
 import sys
+from argparse import ArgumentParser
+from collections.abc import Sequence
+from pathlib import Path
 
 
 def main() -> int:
-    ...
+    args = parse_args()
+
+    retval = 0
+    for file in args.files:
+        content = file.read_text()
+        new_content = content.replace('http://', 'https://')
+        if new_content != content:
+            file.write_text(new_content)
+            retval = 1
+
+    return retval
+
+
+class Args(argparse.Namespace):
+    files: Sequence[Path]
+
+
+def parse_args() -> Args:
+    parser = ArgumentParser('enforce-https')
+
+    parser.add_argument(
+        'files',
+        nargs='+',
+        type=Path,
+    )
+
+    return parser.parse_args(namespace=Args())
 
 
 if __name__ == '__main__':
